b1c62b26d54c728ebb34a6e6faa789e9f5f7d37b
crissvinces <cvinces@espol.edu.ec>
2023-11-06 14:47:06 -0500

    Vista Inicio de sesion

---
+++ Proyecto-Estructuras/src/main/java/ejerc/proyecto/estructuras/App.java
@@ -0,0 +1,63 @@
+package ejerc.proyecto.estructuras;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button btn_registrarse= new Button(" Registrarse ");
+        Button btn_inicio=new Button(" Iniciar Sesión ");
+        Label lbl_cuenta= new Label("Aun no tienes una cuenta?");
+        Font fuente_lbl = Font.font("Verdana", FontWeight.EXTRA_BOLD,10);
+        lbl_cuenta.setFont(fuente_lbl);
+        BorderPane root = new BorderPane();
+        Label lbl_menu= new Label("Lista De Contactos");
+        Font fuente = Font.font("Verdana", FontWeight.EXTRA_BOLD, 25);
+        lbl_menu.setFont(fuente);
+        BorderPane.setAlignment(lbl_menu, Pos.CENTER);
+        VBox btn_botones= new VBox();
+        btn_botones.getChildren().addAll(btn_inicio,btn_registrarse,lbl_cuenta);
+        btn_botones.setAlignment(Pos.CENTER);
+        btn_botones.setSpacing(10);
+        Button btn_salir = new Button("X Salir");
+        btn_salir.setStyle("-fx-background-color: red; -fx-text-fill: white;");
+//        FileInputStream input = new FileInputStream("Imagenes/Imagen menu contactos.png");
+//        Image image = new Image(input);
+//        ImageView imageView = new ImageView("image");
+//        root.getChildren().add(imageView);
+        root.setTop(lbl_menu);
+        root.setBottom(btn_salir);
+        root.setCenter(btn_botones);
+        btn_salir.setOnMouseClicked(e -> stage.close());
+        Scene scene = new Scene(root, 600, 300);
+        stage.setTitle("Inicio");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ Proyecto-Estructuras/src/main/java/ejerc/proyecto/estructuras/Registro.java
@@ -0,0 +1,45 @@
+package ejerc.proyecto.estructuras;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author crist
+ */
+public class Registro extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        TextField txt_nombre= new TextField();
+        Button btn = new Button();
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
