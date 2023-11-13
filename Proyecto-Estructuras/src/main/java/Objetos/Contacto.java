39e785c564d265ece3533a8b76b9d35297f200cf
joaaguer <joaaguer@espol.edu.ec>
2023-11-13 15:34:52 -0500

    Creacion de clases contacto y contactoEmpresa
    nn

---
+++ Proyecto-Estructuras/src/main/java/Objetos/Contacto.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objetos;
+
+import espol.utilidades.ArrayList;
+import java.util.Date;
+import espol.utilidades.List;
+import java.io.Serializable;
+/**
+ *
+ * @author Lenovo
+ */
+public class Contacto implements Serializable{
+    private String nombre;
+    private String numeroTelefono;
+    private String foto;
+    private String direccion;
+    private String emailPersonal;
+    private List<String> numerosTelefonicos;
+    private List<String> redesSociales;
+    private Date fechaCumpleanos;
+    private List<Contacto> contactosRelacionados;
+
+    // Constructor
+    public Contacto(String nombre, String numeroTelefono, String foto, String direccion, String emailPersonal) {
+        this.nombre = nombre;
+        this.numeroTelefono = numeroTelefono;
+        this.foto = foto;
+        this.direccion = direccion;
+        this.emailPersonal = emailPersonal;
+        this.numerosTelefonicos = new ArrayList<>();
+        this.redesSociales = new ArrayList<>();
+        this.contactosRelacionados = new ArrayList<>();
+    }
+
+    // Métodos getter y setter (puedes generarlos automáticamente en tu IDE)
+    
+    // Otros métodos según sea necesario
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNumeroTelefono() {
+        return numeroTelefono;
+    }
+
+    public void setNumeroTelefono(String numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmailPersonal() {
+        return emailPersonal;
+    }
+
+    public void setEmailPersonal(String emailPersonal) {
+        this.emailPersonal = emailPersonal;
+    }
+
+    public List<String> getNumerosTelefonicos() {
+        return numerosTelefonicos;
+    }
+
+    public void setNumerosTelefonicos(List<String> numerosTelefonicos) {
+        this.numerosTelefonicos = numerosTelefonicos;
+    }
+
+    public List<String> getRedesSociales() {
+        return redesSociales;
+    }
+
+    public void setRedesSociales(List<String> redesSociales) {
+        this.redesSociales = redesSociales;
+    }
+
+    public Date getFechaCumpleanos() {
+        return fechaCumpleanos;
+    }
+
+    public void setFechaCumpleanos(Date fechaCumpleanos) {
+        this.fechaCumpleanos = fechaCumpleanos;
+    }
+
+    public List<Contacto> getContactosRelacionados() {
+        return contactosRelacionados;
+    }
+
+    public void setContactosRelacionados(List<Contacto> contactosRelacionados) {
+        this.contactosRelacionados = contactosRelacionados;
+    }
+}
--- Proyecto-Estructuras/src/main/java/Objetos/ContactoEmpresa.java
+++ Proyecto-Estructuras/src/main/java/Objetos/ContactoEmpresa.java
@@ -8,6 +8,32 @@ package Objetos;
  *
  * @author Lenovo
  */
-public class ContactoEmpresa {
+public class ContactoEmpresa extends Contacto{
+    private Contacto contactoDirector;
+    private Contacto contactoAsistente;
+
+    // Constructor
+    public ContactoEmpresa(String nombre, String numeroTelefono, String foto, String direccion, String emailPersonal) {
+        super(nombre, numeroTelefono, foto, direccion, emailPersonal);
+        this.contactoDirector = null;
+        this.contactoAsistente = null;
+    }
+
+    public Contacto getContactoDirector() {
+        return contactoDirector;
+    }
+
+    public void setContactoDirector(Contacto contactoDirector) {
+        this.contactoDirector = contactoDirector;
+    }
+
+    public Contacto getContactoAsistente() {
+        return contactoAsistente;
+    }
+
+    public void setContactoAsistente(Contacto contactoAsistente) {
+        this.contactoAsistente = contactoAsistente;
+    }
+    
     
 }
